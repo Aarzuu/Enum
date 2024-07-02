55fc0f20b9e16596f1ba4377a70d5e70ea4de207
Arzu <arzu32ceferova@gmail.com>
2024-07-02 19:07:26 +0400

    enums

--- src/Main.java
+++ src/Main.java
@@ -9,30 +9,31 @@ public class Main {
         System.out.println("Enter the name of any day of the week:");
         String day = sc.next();
         if (Objects.equals(day, daysofweek.Monday.name())) {
-            System.out.println(daysofweek.Tuesday);
+            System.out.println(daysofweek.Monday.getNextDay());
         }
         else if (Objects.equals(day, daysofweek.Tuesday.name())) {
-            System.out.println(daysofweek.Wednesday);
+            System.out.println(daysofweek.Tuesday.getNextDay());
         }
         else if (Objects.equals(day, daysofweek.Wednesday.name())) {
-            System.out.println(daysofweek.Thursday);
+            System.out.println(daysofweek.Wednesday.getNextDay());
         }
         else if (Objects.equals(day, daysofweek.Thursday.name())) {
-            System.out.println(daysofweek.Friday);
+            System.out.println(daysofweek.Thursday.getNextDay());
         }
         else if (Objects.equals(day, daysofweek.Friday.name())){
-            System.out.println(daysofweek.Saturday);;
+            System.out.println(daysofweek.Friday.getNextDay());;
         }
         else if (Objects.equals(day, daysofweek.Saturday.name())){
-            System.out.println(daysofweek.Sunday);
+            System.out.println(daysofweek.Saturday.getNextDay());
         }
         else if (Objects.equals(day, daysofweek.Sunday.name())){
-            System.out.println(daysofweek.Monday);
+            System.out.println(daysofweek.Sunday.getNextDay());
         }
         else {
             System.out.println("this is not a day of week");
             System.out.println("Or you type it wrong");
         }
+
 
         //1.3.Müəyyən bir günün iş günü və ya həftə sonu olub olmadığını yoxlayın
             if (Objects.equals(day, daysofweek.Monday.name()) || Objects.equals(day, daysofweek.Tuesday.name()) || Objects.equals(day, daysofweek.Wednesday.name()) || Objects.equals(day, daysofweek.Thursday.name()) || Objects.equals(day, daysofweek.Friday.name())){
--- src/daysofweek.java
+++ src/daysofweek.java
@@ -1,9 +1,19 @@
 public enum daysofweek {
-    Monday,
-    Tuesday,
-    Wednesday,
-    Thursday,
-    Friday,
-    Saturday,
-    Sunday
+    Monday("the next day is Tuesday"),
+    Tuesday("the next day is Wednesday"),
+    Wednesday("the next day is Thursday"),
+    Thursday("the next day is Friday"),
+    Friday("the next day is Saturday"),
+    Saturday("the next day is Sunday"),
+    Sunday("the next day is Monday");
+
+    private String nextDay;
+
+    daysofweek(String nextDay) {
+        this.nextDay = nextDay;
+    }
+
+    public String getNextDay() {
+        return nextDay;
+    }
 }
